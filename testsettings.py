803c71dfb935e5a250dd9835f87bbf7c4493de1f
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-03-06 16:16:41 -0500

    Initial commit
    This project structure for this app is based on DabApps' django-reusable-app template at
    https://github.com/dabapps/django-reusable-app. The HTTP proxy is pulled from OpenPlans'
    Shareabouts client starter at https://github.com/openplans/shareabouts-django-client.

---
+++ testsettings.py
@@ -0,0 +1,11 @@
+SECRET_KEY = '12345'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    },
+}
+
+INSTALLED_APPS = (
+    'proxy',
+)
